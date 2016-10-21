14db519da8ac1fe226a3206d5cb20233a56f5941
Sathyamurugesan <sathiya8297@gmail.com>
2016-10-21 22:51:31 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a;
+  clrscr();
+  printf("Enter a number:");
+  scanf("%d",&a);
+  if(a>0)
+  {
+    printf("The number is positive");
+  }
+  else if(a<0)
+  {
+    printf("The number is negative");
+  }
+  else
+  {
+    printf("The number is zero");
+  }
+  getch();
+}
